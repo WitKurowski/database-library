67135c5695e524b87c832788060feee6d7d0e2ea
Wit Kurowski <witkurowski@gmail.com>
2015-02-02 22:20:19 -0800

    SQLite INTEGERs can be up to 8-bytes large.

--- DatabaseLibrary/src/main/java/com/wit/databaselibrary/model/DatabaseObject.java
+++ DatabaseLibrary/src/main/java/com/wit/databaselibrary/model/DatabaseObject.java
@@ -1,13 +1,13 @@
 package com.wit.databaselibrary.model;
 
 public class DatabaseObject {
-	private final Integer id;
+	private final Long id;
 
-	public DatabaseObject( final Integer id ) {
+	public DatabaseObject( final Long id ) {
 		this.id = id;
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return this.id;
 	}
 }
