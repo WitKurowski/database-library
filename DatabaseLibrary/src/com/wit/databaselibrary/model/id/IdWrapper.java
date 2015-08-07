b18036dd617a2d8f937b9965d2d957be15faa11f
Wit Kurowski <witkurowski@gmail.com>
2015-08-07 09:31:21 -0700

    The IdWrapper classes are no longer used.

--- DatabaseLibrary/src/com/wit/databaselibrary/model/id/IdWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.wit.databaselibrary.model.id;
-
-import com.wit.databaselibrary.model.DatabaseObject;
-
-/**
- * Wraps the ID(s) of a {@link DatabaseObject} for comparison purposes.
- */
-public interface IdWrapper {
-}
