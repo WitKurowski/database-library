994f2c1edd31a62928a29a60e92a0be6d09d944e
Wit Kurowski <witkurowski@gmail.com>
2015-11-03 13:43:55 -0800

    Attempting to update an already deleted object caused a NullPointerException in Manager.performUpdate(T).

--- DatabaseLibrary/src/main/java/com/wit/databaselibrary/service/Manager.java
+++ DatabaseLibrary/src/main/java/com/wit/databaselibrary/service/Manager.java
@@ -586,8 +586,9 @@ public abstract class Manager<T extends DatabaseObject> {
 	 * @return The updated {@link DatabaseObject} from local storage or null if no update was done.
 	 * @throws IllegalArgumentException The given {@link DatabaseObject} is managed internally and it is out of sync
 	 * with local storage.
+	 * @throws IllegalStateException The given {@link DatabaseObject} has already been deleted from local storage.
 	 */
-	private T performUpdate( final T object ) throws IllegalArgumentException {
+	private T performUpdate( final T object ) throws IllegalArgumentException, IllegalStateException {
 		final Contract contract = this.getContract();
 		final String authority = this.getAuthority();
 		final long id = object.getId();
@@ -605,21 +606,26 @@ public abstract class Manager<T extends DatabaseObject> {
 			whereArgs.add( String.valueOf( object.getVersion() ) );
 		} else {
 			final T savedObject = this.get( id );
-			final long savedObjectVersion = savedObject.getVersion();
-			final long newObjectVersion = object.getVersion();
 
-			if ( newObjectVersion < savedObjectVersion ) {
-				throw new IllegalArgumentException(
-						"Attempting to update a stale object.  The stored version of the object is '" +
-								savedObjectVersion + "' while the passed in object has a version of '" +
-								newObjectVersion + "'." );
+			if ( savedObject == null ) {
+				throw new IllegalStateException( "Attempting to update a deleted object with: " + object );
+			} else {
+				final long savedObjectVersion = savedObject.getVersion();
+				final long newObjectVersion = object.getVersion();
+
+				if ( newObjectVersion < savedObjectVersion ) {
+					throw new IllegalArgumentException(
+							"Attempting to update a stale object.  The stored version of the object is '" +
+									savedObjectVersion + "' while the passed in object has a version of '" +
+									newObjectVersion + "'." );
+				}
+
+				whereClauseBuilder.append( " AND " + Contract.Columns.VERSION + " = ?" );
+
+				whereArgs.add( String.valueOf( savedObjectVersion ) );
+
+				object.setVersion( newObjectVersion + 1 );
 			}
-
-			whereClauseBuilder.append( " AND " + Contract.Columns.VERSION + " = ?" );
-
-			whereArgs.add( String.valueOf( savedObjectVersion ) );
-
-			object.setVersion( newObjectVersion + 1 );
 		}
 
 		final Uri contentUri = contract.getContentUri( authority, id );
